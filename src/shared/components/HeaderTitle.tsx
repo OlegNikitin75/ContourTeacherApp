d85fe3bd8f111063a8c1cbae9b3be89f548dd665
OlegNikitin75 <nov075@mail.ru>
2026-05-26 11:02:05 +0300

    refactor: changed the native wind styles to react native styles in the headertitle

--- src/shared/components/HeaderTitle.tsx
+++ src/shared/components/HeaderTitle.tsx
@@ -1,15 +1,36 @@
-import { Text, View } from 'react-native'
+import { colors, spacing, typography } from '@/core/constants/theme'
+import React from 'react'
+import { Text, View, StyleSheet } from 'react-native'
 
 interface HeaderTitleProps {
 	firstItemTitle: string
 	secondItemTitle: string
 }
+
 export default function HeaderTitle({ firstItemTitle, secondItemTitle }: HeaderTitleProps) {
 	return (
-		<View className='flex-row items-center justify-center gap-1 py-4'>
-			<Text className=' text-h3 text-app-black'>{firstItemTitle}</Text>
-			<Text className='text-h3 text-app-black '>|</Text>
-			<Text className='text-h3 accent-color '>{secondItemTitle}</Text>
+		<View style={styles.container}>
+			<Text style={styles.textBlack}>{firstItemTitle}</Text>
+			<Text style={styles.textBlack}>|</Text>
+			<Text style={styles.textAccent}>{secondItemTitle}</Text>
 		</View>
 	)
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'center',
+		gap: spacing(1),
+		paddingVertical: spacing(4),
+	},
+	textBlack: {
+		...typography.h3,
+		color: colors.appBlack,
+	},
+	textAccent: {
+		...typography.h3,
+		color: colors.appAccent, 
+	},
+})
